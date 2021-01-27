e36108d3c56c9df8eddbda17f1ab7dde20f25363
nishan8583 <nishanmaharjan17@gmail.com>
2021-01-27 00:51:06 -0600

    timeout ERROR

--- sshlib/executeCommands.go
+++ sshlib/executeCommands.go
@@ -1,20 +1,23 @@
 package sshlib
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 // ExecuteCommand executs the command provided, and returns the string output and error if any
-func (s *SSH) ExecuteCommand(comamnd string) (string, error) {
+func (s *SSH) ExecuteCommand(comamnd string, timeout time.Duration) (string, error) {
 	_, err := fmt.Fprintf(s.input, "%s\n", comamnd)
 	if err != nil {
 		return "", err
 	}
 
-	err = s.session.Wait()
-	if err != nil {
-		return "", err
-	}
-
 	output := s.outputBuffer.String()
+	if len(output) == 0 {
+		time.Sleep(timeout * time.Second)
+
+		output = s.outputBuffer.String(
+	}
 	s.outputBuffer.Reset()
 
 	return output, nil
